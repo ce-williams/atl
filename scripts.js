4b19fde0ac9e534eda21cf9642ff353977358181
Alec Dewitz <atdewitz@csbsju.edu>
2018-03-03 23:29:45 -0600

    This cleans up the code a little better

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
 var text = prompt('paste your font here and it will display on the page');
-var lower = text.toLowerCase();
 alert('click to get font');
-document.write(lower);
+
+if (text) {
+    text = text.toLowerCase();
+    document.getElementById('outputText').innerHTML = text;
+} else {
+    document.getElementById('outputText').innerHTML = "NONE!!"
+}
